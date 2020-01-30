ec6f3352ff027ce16fbf50880704de2eabe4d38f
Zsuzsanna Palfi <zsuzsanna.palfi@cognitivecreators.com>
2020-01-30 17:05:50 +0200

    started reactjs education

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ import CountPresenter from './Components/CountPresenter/CountPresenter';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      count: 0,
-    }
+    this.test = {date: new Date()};
+    this.state = {count: 0};
   }
 
   increaseCount() {
     this.state.count = this.state.count + 1;
   }
 
+  OnClickButton = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  };
+
   render() {
     return (
-      <button>
+      <div>
+         <button onClick={this.OnClickButton}>
         Increase count
       </button>
-      <CountPresenter />
+
+      {this.state.count}
+   <CountPresenter></CountPresenter>
+      <div>
+        <h1>Just test</h1>
+        <h2>It is {this.test.date.toLocaleTimeString()}.</h2>
+      </div>
+      </div>
     );
   }
 }
