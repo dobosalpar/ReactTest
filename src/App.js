7f39dc73b74efc57daf81df7ace213741384bb49
Kazmer Mate <kazmer.mate@icloud.com>
2020-02-10 17:07:55 +0200

    Fixed: constructor and increaseCount function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CountPresenter from './Components/CountPresenter/CountPresenter';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+
+        this.increaseCount = this.increaseCount.bind(this);
     }
-  }
 
-  increaseCount() {
-    this.state.count = this.state.count + 1;
-  }
+    increaseCount() {
+        this.setState((state) => ({
+            count: state.count + 1
+        }));
+    }
 
-  render() {
-    return (
-      <button>
-        Increase count
-      </button>
-      <CountPresenter />
-    );
-  }
+    render() {
+        return (
+            <button onClick={this.increaseCount}>
+                <CountPresenter count={this.state.count}/>
+            </button>
+        );
+    }
 }
 
 export default App;
