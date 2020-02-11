cce06e33373b1b9278927f2b53c67ccfd9ca6f01
Alpar Dobos <dobosalpar10@gmail.com>
2020-02-11 08:47:34 +0200

    SOL: 1

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ class App extends Component {
     this.state = {
       count: 0,
     }
+
+    // To avoid unnecessary renders, this has to be bound to the scope of the parent (App) component
+    this.increaseCount = this.increaseCount.bind(this);
   }
 
   increaseCount() {
-    this.state.count = this.state.count + 1;
+    // The state shoud be never mutated, a new instance of state the has to be set by using the setState function. 
+    this.setState({
+      count: this.state.count + 1,
+    });
   }
 
   render() {
     return (
-      <button>
-        Increase count
-      </button>
-      <CountPresenter />
+      // The whole UI had to be embeded into a fragment since we are returning multiple elements at the same DOM level. https://reactjs.org/docs/react-api.html#reactfragment
+      <>
+        {/* Add increaseCount as the click event handler for the button  */}
+        <button onClick={this.increaseCount}>
+          Increase count
+        </button>
+        {/* State had to be passed as a prop to the CountPresenter component  */}
+        <CountPresenter count={this.state.count} />
+      </>
     );
   }
 }
