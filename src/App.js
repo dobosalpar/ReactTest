25ff42fc6fd34ca5b312b8c5867db998cdc56aa7
Alpar Dobos <dobosalpar10@gmail.com>
2020-02-12 09:38:25 +0200

    SOL 1

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ class App extends Component {
     this.state = {
       count: 0,
     }
+    // Increase count function is bound to the the scope of the App to avoid extra renders, and to use the propter this when called. 
+    this.increaseCount = this.increaseCount.bind(this);
   }
 
   increaseCount() {
-    this.state.count = this.state.count + 1;
+    // State should be never mutated. state updates are asyncrounous, callback function needs to be used, to make sure that we are updating the proper state
+    this.setState(({ count }) => ({
+      count: count +1,
+    }))
+
   }
 
   render() {
     return (
-      <button>
-        Increase count
-      </button>
-      <CountPresenter />
+      // We cannot return multiple elements at the same level. To avoid extra divs in the 'real' DOM, we have to embed them into fragments.
+      <>
+        {/* Button receives the increaseCount as a click event handler */}
+        <button onClick={this.increaseCount}>
+          Increase count
+        </button>
+        {/* The state is passed as prop to the CountPresenter component */}
+        <CountPresenter count={this.state.count} />
+      </>
     );
   }
 }
