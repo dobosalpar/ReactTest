80828a26b229af5da71fae5c39fb6eed338c20f8
Zsuzsanna Palfi <zsuzsanna.palfi@cognitivecreators.com>
2020-02-12 10:32:52 +0200

    the state passed as props, using bind the method in the constructor, changed method name

--- src/App.js
+++ src/App.js
@@ -6,27 +6,31 @@ class App extends Component {
     super();
     this.test = {date: new Date()};
     this.state = {count: 0};
+    this.onClickButton = this.onClickButton.bind(this);
   }
 
-  increaseCount() {
+  /*increaseCount() {
     this.state.count = this.state.count + 1;
-  }
+  }*/
 
-  OnClickButton = () => {
+  //onClickButton = () => {   //   This works, but this is an experimental solution. It is advised to bind the method in the constructor.
+  onClickButton() {
     this.setState(({ count }) => ({
       count: count + 1
     }));
   };
 
+  // method names are starting with a small case
+
+  ////The state has to be passed as props to the CountPresenter component. I know it wasn't made clear in the description :|
   render() {
     return (
       <div>
-         <button onClick={this.OnClickButton}>
+         <button onClick={this.onClickButton}>
           Increase count
         </button>
-
-        {this.state.count}
-        <CountPresenter />
+     
+        <CountPresenter count={this.state.count} />
         <div>
           <h1>Just test</h1>
           <h2>It is {this.test.date.toLocaleTimeString()}.</h2>
