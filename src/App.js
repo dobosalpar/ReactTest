c3390ecab6e3991c6fd050b14ebe02dd070a61fb
Alpar Dobos <dobosalpar10@gmail.com>
2020-02-14 14:26:35 +0200

    EX: 2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react';
-import CountPresenter from './Components/CountPresenter/CountPresenter';
+import List from './Components/List/List';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      count: 0,
-    }
-    // Increase count function is bound to the the scope of the App to avoid extra renders, and to use the propter this when called. 
-    this.increaseCount = this.increaseCount.bind(this);
+    this.downloadList = this.downloadList.bind(this);
   }
 
-  increaseCount() {
-    // State should be never mutated. state updates are asyncrounous, callback function needs to be used, to make sure that we are updating the proper state
-    this.setState(({ count }) => ({
-      count: count +1,
-    }))
-
+  downloadList() {
+    
   }
 
   render() {
     return (
-      // We cannot return multiple elements at the same level. To avoid extra divs in the 'real' DOM, we have to embed them into fragments.
       <>
-        {/* Button receives the increaseCount as a click event handler */}
-        <button onClick={this.increaseCount}>
-          Increase count
+        <button onClick={this.downloadList}>
+          Download list
         </button>
-        {/* The state is passed as prop to the CountPresenter component */}
-        <CountPresenter count={this.state.count} />
+        <List />
       </>
     );
   }
