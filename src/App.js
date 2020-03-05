003a3789513be40bb903cfa49fd72572b1c4a122
Alpar Dobos <dobosalpar10@gmail.com>
2020-03-05 11:34:33 +0200

    SOL 2

--- src/App.js
+++ src/App.js
@@ -4,20 +4,37 @@ import List from './Components/List/List';
 class App extends Component {
   constructor() {
     super();
+    // This is called moving the state up. The List component's state was moved here, since both the button and List has to know about it.
+    this.state = {
+      isLoading: false,
+      list: [],
+    };
     this.downloadList = this.downloadList.bind(this);
   }
 
   downloadList() {
-    
+    this.setState({
+			isLoading: true,
+		})
+		fetch('https://jsonplaceholder.typicode.com/posts')
+  			.then(response => response.json())
+			.then(json => {
+				this.setState({
+					isLoading: false,
+					list: json,
+				})
+			});
   }
 
   render() {
+    const { isLoading, list } = this.state;
     return (
       <>
-        <button onClick={this.downloadList}>
+        <button onClick={this.downloadList} disabled={isLoading}>
           Download list
         </button>
-        <List />
+        {/* The this values were part of the List component's state now they are passed down as props */}
+        <List isLoading={isLoading} list={list} />
       </>
     );
   }
--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -2,33 +2,14 @@ import React, { Component } from 'react';
 import './List.css';
 
 class List extends Component {
-	constructor() {
-		super();
-		this.state = {
-			isLoading: false,
-			list: [],
-		}
-	}
-
-	componentDidMount() {
-		this.setState({
-			isLoading: true,
-		})
-		fetch('https://jsonplaceholder.typicode.com/posts')
-  			.then(response => response.json())
-			.then(json => {
-				this.setState({
-					isLoading: false,
-					list: json,
-				})
-			});
-	}
-
 	render() {
+		const { isLoading, list } = this.props;
 		return (
 			<div className="list">
-				<div>Loading...</div>
-				{this.state.list.map(element => (
+
+				{/* Conditional render is used to present the loading state */}
+				{isLoading && <div>Loading...</div>}
+				{list.map(element => (
 					<div className="list-element">
 						<div className="list-element__title">{element.title}</div>
 						<div className="list-element__content">{element.body}</div>
