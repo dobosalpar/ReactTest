3fe33f5d67e20e60fca22eb2df0bbbac759728fb
Attila <smattila33@gmail.com>
2020-03-10 19:03:15 +0200

    Age guesser functionality added

--- src/Screens/AgeGuesser/AgeGuesser.jsx
+++ src/Screens/AgeGuesser/AgeGuesser.jsx
@@ -4,25 +4,44 @@ class AgeGuesser extends Component {
   constructor() {
     super();
     this.state = {
-      name: ''
+      name: '',
+      isLoading: false,
+      guessedAge: ''
     };
     this.guessAge = this.guessAge.bind(this);
+    this.handleChange = this.handleChange.bind(this)
   }
 
   guessAge() {
-    const name = 'Alpar';
-    fetch(`https://api.agify.io?name=${name}`)
+    this.setState({
+      isLoading: true
+    })
+    fetch(`https://api.agify.io?name=${this.state.name}`)
       .then(response => response.json())
       .then(json => {
-        console.log(json);
+        this.setState({
+          isLoading: false,
+          guessedAge: json.age
+        })
       });
+  }
+
+  handleChange (e) {
+    this.setState({
+      name: e.target.value,
+      guessedAge: ""
+    });
   }
 
   render() {
     return (
       <>
         <h1>Age Guesser</h1>
-        <button onClick={this.guessAge}>Guess Age</button>
+        <input onChange={this.handleChange}/>
+        <br/>
+        <button onClick={this.guessAge} disabled={this.state.name === '' || this.state.isLoading}>Guess Age</button>
+        <br/>
+        {this.state.guessedAge && <label>Guessed age for {this.state.name} is {this.state.guessedAge}</label>}
       </>
     );
   }
