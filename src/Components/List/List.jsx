69a6077002e433834afd367d4426ee6617c91ff5
Attila <smattila33@gmail.com>
2020-04-18 18:03:50 +0300

    Post detail screen connected

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -3,14 +3,14 @@ import './List.css';
 
 class List extends Component {
 	render() {
-		const { isLoading, list } = this.props;
+		const { isLoading, list, onClick } = this.props;
 		return (
 			<div className="list">
-
+				
 				{/* Conditional render is used to present the loading state */}
 				{isLoading && <div>Loading...</div>}
 				{list.map(element => (
-					<div key={element.id} className="list-element">
+					<div key={element.id} className="list-element" onClick = {() => {onClick(element)}}>
 						<div className="list-element__title">{element.title}</div>
 						<div className="list-element__content">{element.body}</div>
 					</div>
