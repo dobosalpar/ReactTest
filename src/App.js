242b94523a17976ac8317b8588424babeff4aa7d
nagynorbie <nnorbie@gmail.com>
2020-06-26 12:15:43 +0300

    Fixed counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import CountPresenter from './Components/CountPresenter/CountPresenter';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(prop) {
+    super(prop);
     this.state = {
       count: 0,
-    }
+    };
+    this.increaseCount = this.increaseCount.bind(this);
   }
 
   increaseCount() {
-    this.state.count = this.state.count + 1;
+    this.setState((state) => ({
+     count: state.count + 1
+    }));
   }
 
   render() {
     return (
-      <button>
-        Increase count
-      </button>
-      <CountPresenter />
+      <>
+        <button onClick={(this.increaseCount)}>
+          Increase count
+        </button>
+        <CountPresenter count={this.state.count}/>
+      </>
     );
-  }
+  };
 }
 
 export default App;
