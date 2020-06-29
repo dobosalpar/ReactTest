38e4dbe1793f0779a88bd9a14036d1e110de4d91
RavaszD <ravaszdomi@gmail.com>
2020-06-29 10:32:27 +0300

    Working Loading List and Download button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import List from './Components/List/List';
+import List from './Components/List/List.jsx';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      isLoading: false,
+      list: [],
+    };
     this.downloadList = this.downloadList.bind(this);
   }
-
+   
   downloadList() {
-    
+    this.setState({ 
+      isLoading: true 
+    });
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response=> response.json())
+      .then(json => {
+        this.setState({
+          isLoading: false,
+          list: json,
+        })
+      });   
   }
 
   render() {
+    const { isLoading, list } = this.state;
     return (
       <>
-        <button onClick={this.downloadList}>
-          Download list
+        <button onClick={this.downloadList} disabled={isLoading}>
+          DownloadList
         </button>
-        <List />
+        <List isLoading={isLoading} list={list} />
       </>
-    );
-  }
+    )
+  };
 }
-
 export default App;
--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -2,34 +2,13 @@ import React, { Component } from 'react';
 import './List.css';
 
 class List extends Component {
-	constructor() {
-		super();
-		this.state = {
-			isLoading: false,
-			list: [],
-		}
-	}
-
-	componentDidMount() {
-		this.setState({
-			isLoading: true,
-		})
-		fetch('https://jsonplaceholder.typicode.com/posts')
-  			.then(response => response.json())
-			.then(json => {
-				this.setState({
-					isLoading: false,
-					list: json,
-				})
-			});
-	}
-
 	render() {
+		const { isLoading, list} = this.props;
 		return (
 			<div className="list">
-				<div>Loading...</div>
-				{this.state.list.map(element => (
-					<div className="list-element">
+				{isLoading && <div> Loading...</div>}
+				{list.map(element => (
+					<div className="list-element" key={element.id}>						
 						<div className="list-element__title">{element.title}</div>
 						<div className="list-element__content">{element.body}</div>
 					</div>
