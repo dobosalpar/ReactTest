7b501fba7afa433ade7de286f0a7d97ddd216519
nagynorbie <nnorbie@gmail.com>
2020-06-29 10:39:20 +0300

    Exercise 2

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import List from './Components/List/List';
 class App extends Component {
   constructor() {
     super();
-    this.downloadList = this.downloadList.bind(this);
+    this.state = {
+      isLoading: false,
+      list: [],
+    };
+  this.downloadList = this.downloadList.bind(this);
   }
 
   downloadList() {
-    
+    this.setState({
+      isLoadig: true
+    });
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response=> response.json())
+      .then(json => {
+        this.setState({
+          isLoading:false,
+          list: json,
+        })
+      });
   }
 
   render() {
+    const { isLoading, list} = this.state;
     return (
       <>
-        <button onClick={this.downloadList}>
+        <button onClick={this.downloadList} disabled={isLoading}>
           Download list
         </button>
-        <List />
+        <List isLoading={isLoading} list={list}/>
       </>
     );
   }
